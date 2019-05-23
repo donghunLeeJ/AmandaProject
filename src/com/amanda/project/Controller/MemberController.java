0f7c0a569119d963d5fc3148636b9ed2ffcbceac
dong <sidong91@gmail.com>
2019-05-23 16:41:26 +0900

    modify all

--- src/com/amanda/project/Controller/MemberController.java
+++ src/com/amanda/project/Controller/MemberController.java
@@ -2,6 +2,7 @@ package com.amanda.project.Controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.amanda.project.DAO.MemberDAO;
-<<<<<<< HEAD
 import com.amanda.project.DTO.MemberDTO;
 
-=======
->>>>>>> f7dd6f183ad3758b555c0449b7452bb793303330
+
 
 
 
@@ -22,11 +21,8 @@ public class MemberController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String cmd = request.getRequestURI().substring(request.getContextPath().length()+1);
 		response.setCharacterEncoding("utf-8");
-<<<<<<< HEAD
 		MemberDAO dao=new MemberDAO();
-=======
-		MemberDAO dao = new MemberDAO();
->>>>>>> f7dd6f183ad3758b555c0449b7452bb793303330
+
 		switch(cmd) {
 		
 		case "loginProc.member" :
@@ -73,55 +69,7 @@ public class MemberController extends HttpServlet {
 			
 		case "updateProc.member" :
 			//회원 정보수정 컨트롤러
-<<<<<<< HEAD
-			String pw=request.getParameter("newpw");
-			String email=request.getParameter("email");
-			String phone=request.getParameter("phone");
-			try {
-				int result=dao.updateMember(new MemberDTO(pw,email,phone) );
-				System.out.println(result);
-			} catch (Exception e) {				
-				e.printStackTrace();
-			}	
-=======
-			String id = request.getParameter("newid");
-			String name = request.getParameter("newname");
-			String phone = request.getParameter("newpohone");
-			String email = request.getParameter("newemail");
-			String zipcode = request.getParameter("newpost");
-			String address1 = request.getParameter("newaddress1");
-			String address2 = request.getParameter("newaddress2");
-			int result = 0;
->>>>>>> f7dd6f183ad3758b555c0449b7452bb793303330
-//			if (request.getParameter("newpw") == null) {
-//				String pw = (String) request.getSession().getAttribute("pw");
-//				try {
-//					result = dao.updateMember(new MemberDTO2(id, dao.testSHA256(pw), name, phone, email, zipcode, address1, address2));
-//					request.setAttribute("result",result);
-//					RequestDispatcher rd=request.getRequestDispatcher("WEB-INF/Member/myupdateProc.jsp");
-//					rd.forward(request, response);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//				System.out.print(pw);
-//			} else {
-//				String pw = request.getParameter("newpw");
-//
-//				try {
-//					result = dao.updateMember(new MemberDTO2(id, dao.testSHA256(pw), name, phone, email, zipcode, address1, address2));
-//					request.setAttribute("result",result);
-//					RequestDispatcher rd=request.getRequestDispatcher("WEB-INF/Member/myupdateProc.jsp");
-//					rd.forward(request, response);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//
-//				}
-//				System.out.print(pw);
-<<<<<<< HEAD
-//			}	
-=======
-//			}		
->>>>>>> f7dd6f183ad3758b555c0449b7452bb793303330
+
 			
 			break;
 		}
--- src/com/amanda/project/DAO/MemberDAO.java
+++ src/com/amanda/project/DAO/MemberDAO.java
@@ -1,33 +1,15 @@
 package com.amanda.project.DAO;
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
-<<<<<<< HEAD
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
-import com.amanda.project.DTO.MemberDTO;
-
-public class MemberDAO {
-	private Connection getConnection()throws Exception {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		String url="jdbc:oracle:thin:@localhost:1521:xe";
-		String user="kh";
-		String pw="kh";
-		return DriverManager.getConnection(url,user,pw);
-	}
-	public boolean checklogin(String id,String pw)throws Exception{
-=======
-import java.sql.PreparedStatement;
-
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import com.amanda.project.DTO.MemberDTO;
 
-import com.amanda.project.DTO.MemberDTO2;
 
 public class MemberDAO {
 	DataSource ds;
@@ -41,27 +23,12 @@ public class MemberDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 	public Connection db_connect() throws Exception {
-		return this.ds.getConnection();
+		return ds.getConnection();
 	}
-	public int updateMember(MemberDTO2 dto) throws Exception {
-		String sql="update Members set pw=?,name=?,phone=?,email=?,zipcode=?,address1=?,address2=?  where id=? ";
-		Connection con=this.db_connect();
-		PreparedStatement pstat=con.prepareStatement(sql);
-		pstat.setString(1,dto.getPw());
-		pstat.setString(2,dto.getName());
-		pstat.setString(3, dto.getBirth());
-		pstat.setString(4, dto.getEmail());
-		pstat.setString(5, dto.getPhone());
-		pstat.setString(6, dto.getPhone());
-		pstat.setString(7, dto.getId());	
-		int result=pstat.executeUpdate();
-		con.commit();
-		con.close();
-		return result;
-	}
+
 
 	public static String testSHA256(String str){
 		String SHA = ""; 
@@ -80,46 +47,31 @@ public class MemberDAO {
 		}
 		return SHA;
 	}
->>>>>>> f7dd6f183ad3758b555c0449b7452bb793303330
-
+	public boolean checklogin(String id,String pw)throws Exception{
 		String sql="select * from Members where id='"+id+"'and pw='"+pw+"'";
-		try (	Connection con=this.getConnection();
+		try (	Connection con=ds.getConnection();
 				PreparedStatement pstat=con.prepareStatement(sql);
 				ResultSet rs=pstat.executeQuery();
 				)
 		{return rs.next();}
 	}
-	
-public int updateMember(MemberDTO dto) throws Exception {
-	String sql="update Members set pw=?,email=?,phone=?  where id=? ";
-	Connection con=this.getConnection();
-	PreparedStatement pstat=con.prepareStatement(sql);
-	pstat.setString(1,dto.getPw());
-	pstat.setString(2,dto.getEmail());
-	pstat.setString(3, dto.getPhone());
-	pstat.setString(4, dto.getId());
-		
-	int result=pstat.executeUpdate();
-	con.commit();
-	con.close();
-	return result;
+
+
+
+	public int updateMember(MemberDTO dto) throws Exception {
+		String sql="update Members set pw=?,email=?,phone=?  where id=? ";
+		Connection con=ds.getConnection();
+		PreparedStatement pstat=con.prepareStatement(sql);
+		pstat.setString(1,dto.getPw());
+		pstat.setString(2,dto.getEmail());
+		pstat.setString(3, dto.getPhone());
+		pstat.setString(4, dto.getId());
+
+		int result=pstat.executeUpdate();
+		con.commit();
+		con.close();
+		return result;
+	}
 }
 
-public static String testSHA256(String str){
-	String SHA = ""; 
-	try{
-		MessageDigest sh = MessageDigest.getInstance("SHA-256"); 
-		sh.update(str.getBytes()); 
-		byte byteData[] = sh.digest();
-		StringBuffer sb = new StringBuffer(); 
-		for(int i = 0 ; i < byteData.length ; i++){
-			sb.append(Integer.toString((byteData[i]&0xff) + 0x100, 16).substring(1));
-		}
-		SHA = sb.toString();
-	}catch(NoSuchAlgorithmException e){
-		e.printStackTrace(); 
-		SHA = null; 
-	}
-	return SHA;
-}
-}
+
